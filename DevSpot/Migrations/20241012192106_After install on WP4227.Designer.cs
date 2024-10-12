aad1c3775c177dac2f5f5fcff92fb6814df1800f
Daniel Drion <daniel.drion@inetum-realdolmen.world>
2024-10-12 21:22:32 +0200

    Added migration for WP4227

--- DevSpot/Migrations/20241012192106_After install on WP4227.Designer.cs
+++ DevSpot/Migrations/20241012192106_After install on WP4227.Designer.cs
@@ -4,6 +4,7 @@ using DevSpot.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DevSpot.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241012192106_After install on WP4227")]
+    partial class AfterinstallonWP4227
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
