1b806660aaf33f2b0f17085f0b1b7bc675f41ba9
Daniel Drion <daniel.drion@inetum-realdolmen.world>
2024-10-14 10:50:28 +0200

    Added JobPosting model

---
+++ DevSpot/Models/JobPosting.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DevSpot.Models
+{
+    public class JobPosting
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+    }
+}
