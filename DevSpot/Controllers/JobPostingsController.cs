dad27adffaa29928c5bc72e3ead496e8587ef2e0
Daniel Drion <daniel.drion@inetum-realdolmen.world>
2024-10-15 14:55:32 +0200

    Added job posting form and http post endpoint

--- DevSpot/Controllers/JobPostingsController.cs
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -28,5 +28,11 @@ namespace DevSpot.Controllers
 		{
 			return View();
 		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create(JobPosting jobPosting)
+		{
+			return RedirectToAction(nameof(Index));
+		}
 	}
 }
